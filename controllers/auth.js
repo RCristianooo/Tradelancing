c8a9923292df9ad2ad72c8b63fa3fb54f41f37e2
Cristian <r.cristiano.ai@gmail.com>
2023-06-18 16:56:51 -0400

    Modified and fix bugs in passport.js, auth.js, and logged-header.ejs. Provided code for profile.ejs as a placeholder for now.

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,44 +4,47 @@ const User = require("../models/User")
 
 exports.getLogin = (req, res) => {
     if (req.user) {
-        return res.redirect("/profile   ")
+        return res.redirect("/profile")
     }
     res.render("login", {
         title: "Login",
     });
 };
 
-exports.postLogin = (req, res, next) => {
+exports.postLogin = async (req, res, next) => {
+  try {
     const validationErrors = [];
     if (!validator.isEmail(req.body.email))
-        validationErrors.push({ msg: "Please enter a valid email address."});
-    if(validator.isEmpty(req.body.password))
-        validationErrors.push({ msg: "Password cannot be blank."});
+      validationErrors.push({ msg: "Please enter a valid email address." });
+    if (validator.isEmpty(req.body.password))
+      validationErrors.push({ msg: "Password cannot be blank." });
 
     if (validationErrors.length) {
-        req.flash("errors", validationErrors);
-        return res.redirect("/login");
+      req.flash("errors", validationErrors);
+      return res.redirect("/login");
     }
     req.body.email = validator.normalizeEmail(req.body.email, {
-        gmail_remove_dots: false,
+      gmail_remove_dots: false,
     });
-    
-    passport.authenticate("local", (err, user, info) => {
-        if (err) {
-            return next(err);
-        }
-        if (!user) {
-            req.flash("errors", info);
-            return res.redirect("/login");
-        }
-        req.logIn(user, (err) => {
-            if (err) {
-                return next(err);
-            }
-            req.flash("success", { msg: "Success! You are logged in." });
-            res.redirect(req.session.returnTo || "/profile");
-        });
-    })(req, res, next);
+
+    const user = await new Promise((resolve, reject) => {
+      passport.authenticate("local", (err, user, info) => {
+        if (err) reject(err);
+        if (!user) reject(info);
+        resolve(user);
+      })(req, res, next);
+    });
+
+    req.logIn(user, (err) => {
+      if (err) {
+        return next(err);
+      }
+      req.flash("success", { msg: "Success! You are logged in." });
+      res.redirect(req.session.returnTo || "/profile");
+    });
+  } catch (err) {
+    return next(err);
+  }
 };
 
 exports.logout = (req, res) => {
@@ -65,7 +68,7 @@ exports.getSignup = (req, res) => {
   });
 };
 
-exports.postSignup = (req, res, next) => {
+exports.postSignup = async (req, res, next) => {
   const validationErrors = [];
   if (!validator.isEmail(req.body.email))
     validationErrors.push({ msg: "Please enter a valid email address." });
@@ -92,29 +95,28 @@ exports.postSignup = (req, res, next) => {
     password: req.body.password,
   });
 
-  User.findOne(
-    { $or: [{ email: req.body.email }, { userName: req.body.userName }] },
-    (err, existingUser) => {
+  
+  try {
+    const existingUser = await User.findOne({
+      $or: [{ email: req.body.email }, { userName: req.body.userName }],
+    });
+
+    if (existingUser) {
+      req.flash("errors", {
+        msg: "Account with that email address or username already exists.",
+      });
+      return res.redirect("../signup");
+    }
+
+    await user.save();
+
+    req.logIn(user, (err) => {
       if (err) {
         return next(err);
       }
-      if (existingUser) {
-        req.flash("errors", {
-          msg: "Account with that email address or username already exists.",
-        });
-        return res.redirect("../signup");
-      }
-      user.save((err) => {
-        if (err) {
-          return next(err);
-        }
-        req.logIn(user, (err) => {
-          if (err) {
-            return next(err);
-          }
-          res.redirect("/profile");
-        });
-      });
-    }
-  );
+      res.redirect("/profile");
+    });
+  } catch (err) {
+    return next(err);
+  }
 };
