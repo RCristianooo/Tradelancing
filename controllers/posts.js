036d43edb8eb13b36c9373ea70a38910096f04ec
Cristian <r.cristiano.ai@gmail.com>
2023-06-23 23:15:38 -0400

    Added a couple of modules to be temporarily stored elsewhere in posts.js located in controllers folder for now. Additionally, established pro routes to also be in posts.js in routes folder. Finally, fixed a small bug in professionals.ejs in the views folder.

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,6 +1,7 @@
 const cloudinary = require("../middleware/cloudinary");
 const Post = require("../models/Post");
 const User = require("../models/User")
+const Pro = require("../models/Pro")
 
 module.exports = {
   getProfile: async (req, res) => {
@@ -42,13 +43,13 @@ module.exports = {
   },
   getProfessionals: async (req, res) => {
     try {
-      const posts = await Post.find().sort({ createdAt: "desc" }).lean();
+      const pros = await Pro.find().sort({ createdAt: "desc" }).lean();
       var users = []
-      for (i in posts){
-        var user = await User.findById(posts[i].user)
+      for (i in pros){
+        var user = await User.findById(pros[i].user)
         users.push(user.userName)
       }
-      res.render('professionals.ejs', {posts: posts, userName: users, user: req.user})
+      res.render('professionals.ejs', {pros: pros, userName: users, user: req.user})
     } catch (err) {
       console.log(err)
     }
@@ -83,7 +84,27 @@ module.exports = {
       console.log(err);
     }
   },
+  createPro: async (req, res) => {
+    try {
+      // Upload image to cloudinary
+      const result = await cloudinary.uploader.upload(req.file.path);
 
+      await Pro.create({
+        profession: req.body.profession,
+        location: req.body.location,
+        phoneNumber: req.body.phoneNumber,
+        yearsOfExp: req.body.yearsOfExp,
+        payment: req.body.payment,
+        image: result.secure_url,
+        cloudinaryId: result.public_id,
+        user: req.user.id,
+      });
+      console.log("Pro has been added!");
+      res.redirect("/profile");
+    } catch (err) {
+      console.log(err);
+    }
+  },
   bookmarkJob: async (req, res)=>{
     var bookmarked = false
     try{
@@ -128,6 +149,56 @@ module.exports = {
       await cloudinary.uploader.destroy(post.cloudinaryId);
       // Delete post from db
       await Post.remove({ _id: req.params.id });
+      console.log("Deleted Post");
+      res.redirect("/profile");
+    } catch (err) {
+      res.redirect("/profile");
+    }
+  },
+  bookmarkPro: async (req, res)=>{
+    var bookmarked = false
+    try{
+      var pro = await Pro.findById({_id:req.params.id})
+      bookmarked = (pro.bookmarks.includes(req.user.id))
+    } catch(err){
+    }
+    //if already bookmarked we will remove user from likes array
+    if(bookmarked){
+      try{
+        await Pro.findOneAndUpdate({_id:req.params.id},
+          {
+            $pull : {'bookmarks' : req.user.id}
+          })
+          
+          console.log('Removed user from bookmarks array')
+          res.redirect('back')
+        }catch(err){
+          console.log(err)
+        }
+      }
+      //else add user to bookmarked array
+      else{
+        try{
+          await Pro.findOneAndUpdate({_id:req.params.id},
+            {
+              $addToSet : {'bookmarks' : req.user.id}
+            })
+            
+            console.log('Added user to bookmarks array')
+            res.redirect(`back`)
+        }catch(err){
+            console.log(err)
+        }
+      }
+    },
+  deletePro: async (req, res) => {
+    try {
+      // Find post by id
+      let pro = await Pro.findById({ _id: req.params.id });
+      // Delete image from cloudinary
+      await cloudinary.uploader.destroy(pro.cloudinaryId);
+      // Delete post from db
+      await Pro.remove({ _id: req.params.id });
       console.log("Deleted Post");
       res.redirect("/profile");
     } catch (err) {
--- routes/posts.js
+++ routes/posts.js
@@ -9,10 +9,14 @@ router.get("/:id", ensureAuth, postsController.getJob);
 
 router.post("/createPost", upload.single("file"), postsController.createPost);
 
-router.post("/createPro", upload.single("file"), postsController.createPro)
+router.post("/createPro", upload.single("file"), postsController.createPro);
 
-router.put("/bookmarkJob/:id", postsController.bookmarkJob)
+router.put("/bookmarkJob/:id", postsController.bookmarkJob);
+
+router.put("/bookmarkPro/:id", postsController.bookmarkPro);
 
 router.delete("/deletePost/:id", postsController.deletePost);
+
+router.delete("/deletePro/:id", postsController.deletePro);
 
 module.exports = router;
